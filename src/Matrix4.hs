a4496cf598bb0675130f6b78621c3ee0a0c1cd87
Sergey Krivich <dvdkong2007@gmail.com>
2014-06-28 16:25:46 +0300

    matrix multiplication

--- src/Matrix4.hs
+++ src/Matrix4.hs
@@ -8,20 +8,49 @@ import Vector4
 
 data Matrix4 = Matrix4 [Vector4] deriving (Show)
 
+matrixToList :: Matrix4 -> [[Float]]
+matrixToList (Matrix4 m) = map (vector4ToList) m
+
+matrixFromList :: [[Float]] -> Matrix4
+matrixFromList (m) = Matrix4(map (vector4FromList) m)
+
 column :: Matrix4 -> Int -> Vector4
 column (Matrix4 m) index = m !! index
 
 
 row :: Matrix4 -> Int -> Vector4
-row (Matrix4 m) index = fromList $ map ( !! index) [v1, v2, v3, v4] 
+row (Matrix4 m) index = vector4FromList $ map ( !! index) [v1, v2, v3, v4] 
 	where 
-		v1 = toList $ m !! 0
-		v2 = toList $ m !! 1
-		v3 = toList $ m !! 2
-		v4 = toList $ m !! 3
+		v1 = vector4ToList $ m !! 0
+		v2 = vector4ToList $ m !! 1
+		v3 = vector4ToList $ m !! 2
+		v4 = vector4ToList $ m !! 3
 
---multiply :: Matrix4 -> Matrix4 -> Matrix4
---multiply (Matrix4 m1) (Matrix4 m2)
+
+
+multiply :: Matrix4 -> Matrix4 -> Matrix4
+multiply (Matrix4 m1) (Matrix4 m2) = Matrix4 (
+    [ 
+        Vector4((row m1 0) `dot` (column m2 0)
+                (row m1 0) `dot` (column m2 1)
+                (row m1 0) `dot` (column m2 2)
+                (row m1 0) `dot` (column m2 3)),
+
+        Vector4((row m1 1) `dot` (column m2 0)
+                (row m1 1) `dot` (column m2 1)
+                (row m1 1) `dot` (column m2 2)
+                (row m1 1) `dot` (column m2 3)),
+
+        Vector4((row m1 2) `dot` (column m2 0)
+                (row m1 2) `dot` (column m2 1)
+                (row m1 2) `dot` (column m2 2)
+                (row m1 2) `dot` (column m2 3)),
+
+        Vector4((row m1 3) `dot` (column m2 0)
+                (row m1 3) `dot` (column m2 1)
+                (row m1 3) `dot` (column m2 2)
+                (row m1 3) `dot` (column m2 3)),
+    ])
 
 --multiply :: Vector3 -> Matrix4 -> Vector3
 --multiply 
--- src/Vector4.hs
+++ src/Vector4.hs
@@ -8,16 +8,16 @@ module Vector4
 
 data Vector4 = Vector4 Float Float Float Float deriving (Show)
 
-fromList :: [Float] -> Vector4
-fromList ls = Vector4 x y z w
+vector4FromList :: [Float] -> Vector4
+vector4FromList ls = Vector4 x y z w
 	where 
 		x = ls !! 0
 		y = ls !! 1
 		z = ls !! 2
 		w = ls !! 3
 
-toList :: Vector4 -> [Float]
-toList (Vector4 x1 y1 z1 w1) = [x1, y1, z1, w1]
+vector4ToList :: Vector4 -> [Float]
+vector4ToList (Vector4 x1 y1 z1 w1) = [x1, y1, z1, w1]
 
 dot :: Vector4 -> Vector4 -> Float
 dot (Vector4 x1 y1 z1 w1) (Vector4 x2 y2 z2 w2) = x1*x2 + y1*y2 + z1*z2 + w1*w2
