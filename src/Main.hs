a3c49d6fce43d8dcc6aefabceeddf26edb3ec621
Sergey Krivich <dvdkong2007@gmail.com>
2014-06-29 20:13:36 +0300

    base scene

--- src/Main.hs
+++ src/Main.hs
@@ -3,9 +3,17 @@ import Vector3
 import Matrix4
 import Ray	
 import Scene
+import Figures
 
+render :: Scene -> IO ()
+render scene = return()
 
 main :: IO ()
 main = do
-	return()
+	render $ Scene {
+		sceneElements=[
+				(SceneElement (Sphere 14) (Vector3 0 0 3) (Vector3 1 0 0))
+			]
+		, lights=[]
+	}
 	
